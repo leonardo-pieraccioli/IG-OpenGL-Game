058ab835e73494c45b7ccf392f4de2f255e5446a
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2024-10-25 17:25:11 +0200

    [ADD] RGB flashing when pressing keyboard

--- OpenGLApp/OpenGLApp/Main.cpp
+++ OpenGLApp/OpenGLApp/Main.cpp
@@ -10,6 +10,14 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// window colors
+const float COLOR_BLACK[3] = { 0.0f, 0.0f, 0.0f };
+const float COLOR_RED[3] = { 1.0f, 0.0f, 0.0f };
+const float COLOR_GREEN[3] = { 0.0f, 1.0f, 0.0f };
+const float COLOR_BLUE[3] = { 0.0f, 0.0f, 1.0f };
+
+float currentWindowColor[3];
+
 int main()
 {
     // glfw: initialize and configure
@@ -39,11 +47,16 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        glClearColor(currentWindowColor[0], currentWindowColor[1], currentWindowColor[2], 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -64,4 +77,41 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    
+    // color change
+    // -------------------------------------------------------------------------------------------------
+
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_RELEASE
+        && glfwGetKey(window, GLFW_KEY_G) == GLFW_RELEASE
+        && glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE)
+    {
+        std::copy(std::begin(COLOR_BLACK), std::end(COLOR_BLACK), std::begin(currentWindowColor));
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+    {
+        std::cout << "R pressed" << std::endl;
+        std::copy(std::begin(COLOR_RED), std::end(COLOR_RED), std::begin(currentWindowColor));
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
+    {
+        std::cout << "G pressed" << std::endl;
+        std::copy(std::begin(COLOR_GREEN), std::end(COLOR_GREEN), std::begin(currentWindowColor));
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+    {
+        std::cout << "B pressed" << std::endl;
+        std::copy(std::begin(COLOR_BLUE), std::end(COLOR_BLUE), std::begin(currentWindowColor));
+    }
+
 }
