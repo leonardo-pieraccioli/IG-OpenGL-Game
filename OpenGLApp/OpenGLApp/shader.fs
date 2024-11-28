62c248e2156f0405ca32ef051485be3db747cc73
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2024-11-28 17:29:23 +0100

    [UPDATE] Substitued main files with Example 19

--- OpenGLApp/OpenGLApp/shader.fs
+++ OpenGLApp/OpenGLApp/shader.fs
@@ -1,10 +1,14 @@
 #version 330 core
 out vec4 FragColor;
-  
-uniform vec3 objectColor;
-uniform vec3 lightColor;
+
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+	// linearly interpolate between both textures (80% container, 20% awesomeface)
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
